e334a3c342cf7fccde537b6758c666364362f241
sundaywind <SundayWind@vip.qq.com>
2019-07-03 17:19:46 +0800

    2019年7月3日 17:19:44

--- JDBC/src/wind/com/servlet/SecondJDBC.java
+++ JDBC/src/wind/com/servlet/SecondJDBC.java
@@ -1,8 +1,10 @@
 package wind.com.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -12,25 +14,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/FirstJDBC")
-public class FirstJDBC extends HttpServlet {
+@WebServlet("/SecondJDBC")
+public class SecondJDBC extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+    
 	// 连接数据库的四个参数：
 	private String driver = "com.mysql.jdbc.Driver";
 	private String url = "jdbc:mysql://106.13.108.29:3306/ssms";
 	private String userNamne = "root";
 	private String password = "7758521";
-       
-    public FirstJDBC() {
+		
+    public SecondJDBC() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		Connection con = null;
 		Statement st = null;
 		String msg = "";
+		ResultSet result = null;
 		try {
 			// 1.用反射机制加载MySQL驱动
 			Class.forName(driver);
@@ -39,13 +41,20 @@ public class FirstJDBC extends HttpServlet {
 			// 3.创建运载命令的Statement对象
 			st = con.createStatement();
 			// 4.需要执行的SQL命令
-			String sql = "INSERT INTO USER(id,`account`, `password`, `name`, `type`) VALUES (null, '2019-7-3', '1234567', '金莲妹妹', '2')";
+			String sql = "select * from user";
 			// 5.运载SQL，返回结果
-			int iCount = st.executeUpdate(sql);
-			if (iCount > 0) {
-				msg = "SQLִ执行成功";
-			}else {
-				msg = "SQLִ执行失败";
+			result = st.executeQuery(sql);
+			// 6.获取out对象
+			PrintWriter out = response.getWriter();
+			if (result != null) {
+				while (result.next()) {
+					int row1 = result.getInt(0);
+					String row2 = result.getString(1);
+					String row3 = result.getString(2);
+					String row4 = result.getString(3);
+					String row5 = result.getString(3);
+					out.println("<p>" + row1 + "===" + row2 + "===" + row3 + "===" + row4 + "===" + row5 + "</p>");
+				}
 			}
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
