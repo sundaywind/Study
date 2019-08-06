9cdd0e7e085dd964154b8ae77610eba66ca75c93
sundaywind <SundayWind@vip.qq.com>
2019-08-06 23:07:13 +0800

    完结！

--- JDBC/src/wind/com/servlet/FirstJDBC.java
+++ JDBC/src/wind/com/servlet/FirstJDBC.java
@@ -68,7 +68,6 @@ public class FirstJDBC extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
